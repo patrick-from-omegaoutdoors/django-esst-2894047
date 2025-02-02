63a810bd962455b4b5dc9a8a445e9314205fe21b
Patrick Long <patrick22long@gmail.com>
2025-02-01 21:34:19 -0500

    added home button

--- smartnotes/home/urls.py
+++ smartnotes/home/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view()),
+    path('', views.HomeView.as_view(), name="home"),
     path('authorized', views.AuthorizedView.as_view()),
     path('login', views.LoginInterfaceView.as_view(), name="login"),
     path('logout', views.LogoutInterfaceView.as_view(), name='logout'),
