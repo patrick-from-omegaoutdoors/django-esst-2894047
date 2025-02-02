0ef0b6e02bbf5208fe5072169565b92e43cfa26e
Patrick Long <patrick22long@gmail.com>
2025-02-01 20:22:54 -0500

    add users to notes

--- smartnotes/notes/models.py
+++ smartnotes/notes/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Notes(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     likes = models.PositiveIntegerField(default=0)
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name="notes",
+    )
