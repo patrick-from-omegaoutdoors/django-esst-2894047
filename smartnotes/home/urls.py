7e7632631e79c0360e7cd9e3a8130fa223b176a2
Patrick Long <patrick22long@gmail.com>
2025-02-03 16:05:14 -0500

    register user working

--- smartnotes/home/urls.py
+++ smartnotes/home/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('authorized', views.AuthorizedView.as_view()),
     path('login', views.LoginInterfaceView.as_view(), name="login"),
     path('logout', views.LogoutInterfaceView.as_view(), name='logout'),
+    path('register', views.RegisterInterfaceView.as_view(), name='register'),
 ]
--- smartnotes/home/views.py
+++ smartnotes/home/views.py
@@ -10,9 +10,14 @@
 #     return render(request, 'home/authorized.html', {})
 
 from datetime import datetime
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
+from .forms import UserRegistrationForm
 
 class HomeView(TemplateView):
     template_name = 'home/welcome.html'
@@ -27,3 +32,29 @@ class LoginInterfaceView(LoginView):
 
 class LogoutInterfaceView(LogoutView):
     template_name = "home/logout.html"
+
+class RegisterInterfaceView(CreateView):
+    template_name = "home/register.html"
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy("home")
+    
+    def form_valid(self, form):
+        user = form.save()  # Save the user with hashed password
+        self.object = user  # Set self.object to avoid AttributeError
+
+        username = form.cleaned_data["username"]
+        password = form.cleaned_data["password"]
+
+        user = authenticate(self.request, username=username, password=password)
+
+        if user is not None:
+            login(self.request, user)
+            return HttpResponseRedirect(self.get_success_url())
+        else:
+            form.add_error(None, "Authentication failed. Try logging in.")
+            return self.form_invalid(form)
+
+    def form_invalid(self, form):
+            print("FORM IS INVALID!")  # Debugging output
+            print(form.errors)  # Print form errors to console
+            return super().form_invalid(form)
